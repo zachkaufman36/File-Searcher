c480f6eb573aa744a26c1e872cf21f0b5d70fa0e
Zach Kaufman <zkaufman@filtersense.local>
2022-06-24 16:45:50 -0400

    can now search files and return some. Needs more work overall

--- filesearcher.py
+++ filesearcher.py
@@ -1,9 +1,8 @@
 #File searcher being created for doc control
 import os
-import shutil
-import datetime
 
-path = input('C:\\Users\\zkaufman\\Documents')
+
+path = os.getcwd()
 
 #makes sure it's only grabbing files that we want to be sorted 
 def unsorted(path):
@@ -11,12 +10,24 @@ def unsorted(path):
     files = os.listdir(path)
     return files
 
-#def searching(path, files):
+def search_files(found_files):
 
-
-
-            
+    search = input('What is the name of the file you are searching for? ')
+    for file in range(len(files)):
+        if search in files[file]:
+            found_files.append(files[file])
+    return found_files
 
 if __name__ == '__main__':
+    found_files = []
     files = unsorted(path)
+    files = search_files(found_files)
     print(files)
+    cont = input("Would you like to keep searching? ")
+    if cont == "yes":
+        hunting = search_files(found_files)
+        print(hunting)
+    else:
+        print("Have a nice day")
+        
+    
